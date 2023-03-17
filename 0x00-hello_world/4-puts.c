afffa74d10a8067d0960f7ed086e3ddf35cfb162
SwissHilson <chiderahillary80@gmail.com>
2023-03-17 06:06:00 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - print a program using put function
+ * Return: 0(Program ran successfully!)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilngual puzzle");
+return (0);
+}
