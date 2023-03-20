2b88ad501799fe11a90f7f04656dc729237dc036
SwissHilson <chiderahillary80@gmail.com>
2023-03-20 09:49:46 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,13 @@
  */
 int main(void)
 {
-	int num;
+int num;
 
 	for (num = 0; num <= 9; num++)
-	printf(num);
-	printf('\n');
-	return num;
+	{
+	printf("%d ", num);
+	}
+printf('\n')
+
+return (0);
+}
