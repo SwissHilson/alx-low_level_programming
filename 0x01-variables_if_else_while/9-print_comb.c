ed8e84aec3c3b73bb8a01d905ab5f88954acfab8
SwissHilson <chiderahillary80@gmail.com>
2023-03-22 21:51:02 +0100

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,10 @@ int main(void)
 {
 	int sum;
 
-	for (sum = '0'; sum <= '9'; sum++)
-	{
+	for (sum = '0'; sum <= '9'; sum++){
 		putchar(sum);
 		if (sum == '9')
-			break;
+break;
 		putchar(',');
 		putchar(' ');
 	}
